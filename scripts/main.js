01dbc71c2e6ea8dcb9e57fe9892397dae8d3631c
Kevin Grieger <scuffmark@gmail.com>
2017-05-28 11:59:14 -0500

    dynamically adds charts

--- scripts/main.js
+++ scripts/main.js
@@ -1,60 +1,64 @@
-function highcharts(chartData){
-
-  
 
 
-// Create the chart
-Highcharts.stockChart('container', {
-    chart: {
-        events: {
-            load: function () {
+//get chart data as a promise in a format highcharts can use
+function getChartData(currencyPair){
+    var unixTimeNow = Math.floor(Date.now() / 1000);
+    //why three days? faster load time!
+    var threeDaysAgo = unixTimeNow-3600*24*3;
+    return $.get('https://poloniex.com/public?command=returnChartData&currencyPair=BTC_ETH&start='+threeDaysAgo+'&end=9999999999&period=300').then(function(data){
+        return data.map(function(item){
+            //use base volume for comparing other coins
+            var volume = Math.round(item.volume * 100) / 100
+            //var volume = Math.round(item.quoteVolume * 100) / 100
+            return [item.date * 1000, volume];
+        });
+    })
+}
 
+function createChart(currencyPair, container, chartData){
+    Highcharts.stockChart(container, {
+        chart: {
+            events: {
+                load: function () {
+                    //maby use this for ajax instead? idk...
+                }
             }
-        }
-    },
+        },
 
-    rangeSelector: {
-        buttons: [{
-            count: 60,
-            type: 'minute',
-            text: '1H'
-        }, {
-            count: 1,
-            type: 'day',
-            text: '1D'
-        }, {
-            type: 'all',
-            text: 'All'
-        }],
-        inputEnabled: false,
-        selected: 0
-    },
+        rangeSelector: {
+            buttons: [{
+                count: 60,
+                type: 'minute',
+                text: '1H'
+            }, {
+                count: 1,
+                type: 'day',
+                text: '1D'
+            }, {
+                type: 'all',
+                text: 'All'
+            }],
+            inputEnabled: false,
+            selected: 0
+        },
 
-    title: {
-        text: 'Live random data'
-    },
+        title: {
+            text: currencyPair
+        },
 
-    exporting: {
-        enabled: false
-    },
+        exporting: {
+            enabled: false
+        },
 
-    series: [{
-        name: 'Random data',
-        data:chartData
-    }]
-});
-
-
+        series: [{
+            name: currencyPair,
+            data:chartData
+        }]
+    });
 }
 
 
 $(document).ready(function(){
-
-
-    var unixTimeNow = Math.floor(Date.now() / 1000);
-
-    //why three days? faster load time!
-    var threeDaysAgo = unixTimeNow-3600*24*3;
 
     Highcharts.setOptions({
         global: {
@@ -62,25 +66,32 @@ $(document).ready(function(){
         }
     });
 
-    $.get('https://poloniex.com/public?command=returnChartData&currencyPair=BTC_ETH&start='+threeDaysAgo+'&end=9999999999&period=300',function(data){
-        console.log(data)
-        var chartData = data.map(function(item){
-            //use base volume for comparing other coins
-            var volume = Math.round(item.volume * 100) / 100
-            //var volume = Math.round(item.quoteVolume * 100) / 100
-            return [item.date * 1000, volume];
-        });
-        highcharts(chartData);
+    $('#btnAddChart').click(function(){
+        var currencyPair = $('#selCoins').val();
+        var containerName = currencyPair+'_'+(new Date().getTime());
+        console.log(containerName)
+        var container = `<div id='${containerName}'  style="height: 400px; min-width: 310px"></div>`
+        $('body').append(container);
+        getChartData(currencyPair).then(function(chartData){
+            createChart(currencyPair, containerName, chartData);
+        }); 
     });
 
+    //populate the select box
     $.get('https://poloniex.com/public?command=returnTicker',function(data){
-        var coins = Object.keys(data);
+
+        //sorting in javascript, ugh
+        var coins = Object.keys(data).sort(function(a, b){
+            if(a < b) return -1;
+            if(a > b) return 1;
+            return 0;
+        });
         var options = coins.map(function(coin){
             return `<option value="${coin}">${coin}</option>`
         }).join('');
         $('#selCoins').html(options)
 
-    })
+    });
   
 });
 
